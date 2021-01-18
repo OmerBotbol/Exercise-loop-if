b644f86b333f9ceafec1c579cd3d97ddab65c150
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-18 13:31:58 +0200

    creating index and the first project

---
+++ project1.js
@@ -0,0 +1,7 @@
+let firstNum = Number(prompt("enter the first number"));
+let secondNum = Number(prompt("enter the second number"));
+if (firstNum > secondNum) {
+    alert(firstNum);
+} else{
+    alert(secondNum);
+}
